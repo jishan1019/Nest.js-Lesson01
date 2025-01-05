5336f293d40680f22abc7c57a62594a786e84a08
jishan1019 <imransilamjishan80@gmail.com>
2025-01-05 14:25:28 +0600

    user controller create

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get() // GET /users
+  findAll(@Query('role') role?: 'admin' | 'user') {
+    return this.usersService.findAllFromDB(role);
+  }
+
+  @Get() // GET /users/list
+  findAllList() {
+    return [];
+  }
+
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOneFromDB(id);
+  }
+
+  @Post() // POST /users
+  createUser(@Body() payload: {}) {
+    return payload;
+  }
+
+  @Patch(':id') // PATCH /users/:id
+  updateUser(@Param('id') id: string, @Body() payload: {}) {
+    return {
+      id,
+      ...payload,
+    };
+  }
+
+  @Delete(':id') // Delete /user/:id
+  deleteUser(@Param('id') id: string) {
+    return { id };
+  }
+}
